cde896f6c7ccb23a057202ba200d17cc270dbb9d
MorskiBg <dimityrp@yahoo.com>
2018-11-18 14:56:54 +0200

    Modified usert , added some new props

---
+++ Opereta/Opereta/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Opereta.Models
+{
+    public class Employee : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        [Required]
+        public string PersonalNumber { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Rank { get; set; }
+
+        public DateTime HireDate { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+
+    }
+}
